b046d5ac9143d13892e1cec5b92d3021a5cc0936
Ankit Singhal <ankit.singhal@outlook.com>
2017-03-04 19:49:01 +0530

    Delete InstallWSUS.ps1

--- InstallWSUS.ps1
+++
@@ -1,6 +0,0 @@
-Install-WindowsFeature -Name UpdateServices -IncludeManagementTools
-New-Item -Path D: -Name WsusContent -ItemType Directory
-sleep -Seconds '20'
-cd “C:\Program Files\Update Services\Tools”
-sleep -Seconds '20'
-.\wsusutil.exe postinstall CONTENT_DIR=D:\WsusContent
